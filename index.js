a53e23885f47299baa4fb0ffa9c6bf8dea465fa0
Italo De la Peña <italoandre.1995@gmail.com>
2021-05-02 18:13:11 -0300

    Add Express and endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const notes = [
+let notes = [
   {
     id: 1,
     content: 'Revisar mail',
@@ -21,11 +22,28 @@ const notes = [
   }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  note ? response.json(note) : response.status(404).end()
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
