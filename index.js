20338da95cb5422ce8bb7c131921ab2c3c6aabbf
Italo De la Peña <italoandre.1995@gmail.com>
2021-05-02 14:28:31 -0300

    Add notes data

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const http = require('http')
 
+const notes = [
+  {
+    id: 1,
+    content: 'Revisar mail',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Estudiar',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'Ir a comprar',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text-plain' })
-  response.end('Hello World')
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
 })
 
 const PORT = 3001
